1bde0ee230e5ce80139221608f8e5b75722793e7
John Azar (PC) <johnazar@users.noreply.github.com>
2021-10-16 11:23:24 +0300

    Nuxt Jumpstart

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Nuxt project',
+    title: 'Nuxt Jumpstart',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
